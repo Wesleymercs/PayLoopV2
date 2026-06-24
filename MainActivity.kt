541ae2edcb82010e8a855e5a45fe4ae8b9158be1
Wes <126612628+Wesleymercs@users.noreply.github.com>
2026-06-25 00:46:33 +0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,23 @@
+package com.payloop.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.payloop.app.navigation.PayLoopNavGraph
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                Surface {
+                    PayLoopNavGraph()
+                }
+            }
+        }
+    }
+}
